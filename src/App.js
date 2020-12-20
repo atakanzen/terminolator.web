9e5e67af94592fa0011ba87c26d5e90bcda2acbd
Atakan Zengin <atakan.zengin@modanisa.com>
2020-12-20 23:59:41 +0300

    center info text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
       <div className='flex flex-col items-center h-2/3 space-y-6'>
         <div className="w-3/4 h1/4 p-2 m-2">
-          <p className="text-base font-medium ">the great tool for ending cumbersome process of creating a terminology.</p>
+          <p className="text-base font-medium text-center">the great tool for ending cumbersome process of creating a terminology.</p>
       </div>
       <div className="flex flex-col items-center justify-center bg-gray-100 text-gray-500 shadow rounded-lg w-3/4 h-full cursor-pointer">
       <Dropzone accept=".txt" maxFiles={1} maxSize={5 * 1024 * 1024} onDrop={acceptedFile => {
